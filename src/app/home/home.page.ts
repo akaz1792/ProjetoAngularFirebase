6b705efa403f0e03b90a7e5d33b836040a437142
akaz2407 <akaz_guerra@hotmail.com>
2024-08-23 14:40:14 -0300

    api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,5 @@
+
+
 import { Component } from '@angular/core';
 import { AuthenticateService } from '../services/auth.service';
 import { CrudService } from '../services/crud.service';
@@ -9,7 +11,40 @@ import { MessageService } from '../services/message.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  
+
+  isLoading: boolean = false;
+  funcionarios: any;
+
+  getFuncionarios(){
+    this.isLoading = true;
+	
+		let funcionario = { CodFun: '6' };
+
+    let funcionarios: any;
+
+    fetch('http://localhost/api/v1/listar_funcionarios.php',
+			{
+			  method: 'POST',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(funcionario)
+			}
+		)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      this.funcionarios = response.funcionarios;
+    })
+    .catch(erro => {
+      console.log(erro);
+
+    })
+    .finally(()=>{
+      this.isLoading = false;
+    })
+  }
 
 }
